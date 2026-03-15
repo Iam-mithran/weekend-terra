783268cc336520cce35cf82b857965b3426daa33
Iam-mithran <martinmithran@gmail.com>
2026-03-15 10:57:23 +0530

    create a new qa branch with qa aws resources

---
+++ environments/qa/main.tf
@@ -0,0 +1,17 @@
+module vpc {
+  source = "../../modules/vpc"
+  vpc_cidr = "10.0.0.0/16"
+  subnet_cidr = "10.0.1.0/24"
+  subnet_az = "ap-south-1a"
+  env = "qa"
+}
+
+# Dummy line
+module ec2 {
+  source = "../../modules/ec2"
+  ami_id  = "ami-0f559c3642608c138"
+  ins_type = "t2.micro"
+  sub_id = module.vpc.subnet_id
+  ec2_count = 2
+  environment = "qa"
+}
