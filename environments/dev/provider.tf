2a210824dfe17728bf0ca3f2f7d69d1e43e08c64
Iam-mithran <martinmithran@gmail.com>
2026-03-15 10:08:37 +0530

    Added dev branch and dev env aws resources

---
+++ environments/dev/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+    required_providers {
+        aws = { 
+            source = "hashicorp/aws" 
+            version ="5.0.0" 
+        } 
+    }
+    backend "s3" {
+        bucket = "weekend-terra-15032026" 
+        key    = "environments/dev/terraform.tfstate"
+        region = "ap-south-1"
+    }
+} 
+
+provider "aws" {
+    region = "ap-south-1"
+}
